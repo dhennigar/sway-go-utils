9c46d3fb23bd87dd5e93ea7bbe5d75a7b659b036
dhennigar <danrhennigar@gmail.com>
2023-04-22 10:25:44 -0700

    add bring to ws option

--- sg-launch/sg-launch.go
+++ sg-launch/sg-launch.go
@@ -1,18 +1,37 @@
 // sg-launch.go
 
-// TODO: add function that brings target to current screen if it is open elsewhere (selectable by flag)
-
 package main
 
 import(
-	"os"
 	"strings"
 	"log"
+	"flag"
 
 	"go.i3wm.org/i3/v4"
 )
 
-func focus_or_launch(tree i3.Tree, appid string) {
+func bring_to_current(appid string) {
+
+	tree, err := i3.GetTree()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	current_ws := tree.Root.FindFocused(func(n *i3.Node) bool {
+		return n.Type == i3.WorkspaceNode
+	})
+
+	i3.RunCommand("[app_id=" + appid + "] move to workspace " + current_ws.Name)
+	i3.RunCommand("[instance=" + appid + "] move to workspace " + current_ws.Name)
+}
+
+func focus_or_launch(appid string) {
+
+	tree, err := i3.GetTree()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	child := tree.Root.FindChild(func(n *i3.Node) bool {
 		return strings.HasSuffix(n.AppID, appid) || strings.HasSuffix(n.WindowProperties.Instance, appid)
 	})
@@ -26,12 +45,15 @@ func focus_or_launch(tree i3.Tree, appid string) {
 }
 
 func main() {
-	tree, err := i3.GetTree()
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	target := os.Args[1]
 
-	focus_or_launch(tree, target)		
+	//	var sendFlag = flag.Bool("s", false, "send focused window to target")
+	var bringFlag = flag.Bool("b", false, "bring target to current workspace")
+	flag.Parse()
+	
+	target := flag.Arg(0)
+	
+	if *bringFlag {
+		bring_to_current(target)
+	}
+	focus_or_launch(target)		
 }
